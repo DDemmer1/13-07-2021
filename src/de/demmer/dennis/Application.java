412991dfe8ae1b2831cc0ebf5966f343dc02c832
DDemmer1 <Unknown@DESKTOP-4KSK0J5>
2021-07-13 10:35:36 +0200

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,5 +1,31 @@
 package de.demmer.dennis;
 
-public class Application {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		Queue<String> queue = new LinkedList<>();
+		
+		queue.add("test1");
+		queue.add("test2");
+		queue.add("test3");
+		queue.add("test4");
+		queue.add("test5");
+
+		while(true) {
+			queue.add("test");
+			queue.poll();
+			System.out.println(queue.size());
+		}
+		
+		
+		
+		
+
+	
+		
+	}
 }
